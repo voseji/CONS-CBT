2244a3bd7d2921feafa84f5d475d7cd6a9609ea1
Victor Oseji <vctroseji@gmail.com>
2022-07-22 15:09:43 +0100

    V1

--- src/pages/Dashboard/Results.js
+++ src/pages/Dashboard/Results.js
@@ -1,4 +1,4 @@
-import { Button, FormControl, Grid, Input, InputLabel, MenuItem, Select, Typography } from '@material-ui/core';
+import { Button, FormControl, Grid, Input, InputLabel, MenuItem, Typography } from '@material-ui/core';
 // import React from 'react';
 import React, { useState, useEffect, useRef } from 'react';
 import DashboardTitle from '../../Components/DashboardTitle/DashboardTitle';
@@ -9,6 +9,7 @@ import MUIDataTable from "mui-datatables";
 // import moment from 'moment'
 import { Link } from 'react-router-dom';
 import Icon from '@mui/material/Icon';
+import Select from 'react-select';
 
 const columns = [
 
@@ -18,24 +19,70 @@ const columns = [
     { label: "Phone Number", name: "phoneNumber1" },
     { label: "State Of Origin", name: "stateOfOrigin" },
     { label: "Score", name: "batch" },
-
- 
-
   ];
 
+  const batch = [
 
+    { label: "1A", name: "1A" },
+    { label: "1B", name: "1B" },
+    { label: "1C", name: "1C" },
+    { label: "1D", name: "1D" },
+    { label: "2A", name: "2A" },
+    { label: "2B", name: "2B" },
+  ];
+
+  const sor = [
+    { value: 'ABIA', label: 'ABIA' },
+    { value: 'ADAMAWA', label: 'ADAMAWA' },
+    { value: 'AKWA-IBOM', label: 'AKWA-IBOM' },
+    { value: 'ADAMAWA', label: 'ADAMAWA' },
+    { value: 'ANAMBRA', label: 'ANAMBRA' },
+    { value: 'BAUCHI', label: 'BAUCHI' },
+    { value: 'BAYELSA', label: 'BAYELSA' },
+    { value: 'BENUE', label: 'BENUE' },
+    { value: 'BORNO', label: 'BORNO' },
+    { value: 'CROSS-RIVER', label: 'CROSS-RIVER' },
+    { value: 'DELTA', label: 'DELTA' },
+    { value: 'EBONYI', label: 'EBONYI' },
+    { value: 'EDO', label: 'EDO' },
+    { value: 'EKITI', label: 'EKITI' },
+    { value: 'FCT', label: 'FCT' },
+    { value: 'GOMBE', label: 'GOMBE' },
+    { value: 'IMO', label: 'IMO' },
+    { value: 'JIGAWA', label: 'JIGAWA' },
+    { value: 'KADUNA', label: 'KADUNA' },
+    { value: 'KANO', label: 'KANO' },
+    { value: 'KATSINA', label: 'KATSINA' },
+    { value: 'KEBBI', label: 'KEBBI' },
+    { value: 'KOGI', label: 'KOGI' },
+    { value: 'KWARA', label: 'KWARA' },
+    { value: 'LAGOS', label: 'LAGOS' },
+    { value: 'NASARAWA', label: 'NASARAWA' },
+    { value: 'NIGER', label: 'NIGER' },
+    { value: 'OGUN', label: 'OGUN' },
+    { value: 'ONDO', label: 'ONDO' },
+    { value: 'OSUN', label: 'OSUN' },
+    { value: 'OYO', label: 'OYO' },
+    { value: 'PLATEAU', label: 'PLATEAU' },
+    { value: 'RIVERS', label: 'RIVERS' },
+    { value: 'SOKOTO', label: 'SOKOTO' },
+    { value: 'TARABA', label: 'TARABA' },
+    { value: 'YOBE', label: 'YOBE' },
+    { value: 'ZAMFARA', label: 'ZAMFARA' },
+  ];
 
 // export default class Candidates extends React.Component{
 
   export const Results = () =>{
     // render(){
+      const [selectedOption, setSelectedOption] = useState(null);
 
       const [allregdetails, setAllRegDetails] = useState([])
   useEffect(()=>{
     fetchAllRegDetails()
     },[])
   const fetchAllRegDetails = async () => {
-    await BackendAPI.get(`/students`).then(({data})=>{
+    await BackendAPI.get(`/student/result2`).then(({data})=>{
       // setFacilityType1(data?.data)
       setAllRegDetails(data)
       console.log(data);
@@ -43,8 +90,27 @@ const columns = [
     }
 
         return <DashboardLayout>
+          
             <div>
                 <DashboardTitle title="Candidate Results" />
+                <div style={{width: '300px'}}>
+                <Select
+                styles={{position: 'albsolute', zIndex: '5000'}}
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={sor}
+        placeholder='Filter by State'
+      /><br/></div>
+
+<div style={{width: '300px'}}>
+                <Select
+                styles={{position: 'albsolute', zIndex: '-5000'}}
+        defaultValue={selectedOption}
+        onChange={setSelectedOption}
+        options={batch}
+        placeholder='Filter by Batch'
+      /><br/></div>
+
                 <MUIDataTable 
               columns={columns}
               data={allregdetails.map((registration, index) => [
@@ -55,8 +121,8 @@ const columns = [
                 registration.formNumber,
                 registration.phoneNumber1,
                 registration.stateOfOrigin,
-                registration.batch,
-                registration.registrationNumber,
+                registration.total_score,
+                // registration.registrationNumber,
            
               ])}
              />
